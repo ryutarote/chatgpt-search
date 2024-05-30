295acb2d7af578571a5058c6009442b2e5570ed6
ryutarote <75284855+ryutarote@users.noreply.github.com>
2024-05-30 16:53:05 +0900

    Fix build error

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import preact from '@preact/preset-vite';
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [preact()],
-	base: '/gpt-chat-finder/',
+	base: 'https://ryutarote.github.io/gpt-chat-finder/',
 });
